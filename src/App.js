e3c6536bf129da168b2280ff779bcae0dc82c843
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-10-07 16:37:01 +0300

    Done with randomizing card behavior

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+// Data
+import allCards from "./data";
+
+// Utils
+import { shuffle } from "./utils";
+
+// Components
+import Card from "./Components/Card";
+
+//CSS
 import "./App.css";
 
-//Data
-import cards from "./data";
+const App = () => {
+  const [cards, setCards] = useState(shuffle([...allCards, ...allCards]));
 
-//Components
-import Card from "./Components/Card"; /* 1 */
 
-function App() {
-  let cardsGrid = cards.map(card => (
-    <Card key={card.id} card={card} />
-  )); /* 2, 3 */
+
+  const cardsGrid = cards.map((card, idx) => (
+    <Card key={`${card.id}-${idx}`} card={card} />
+  ));
 
   return (
     <div className="App border my-5">
       <div className="container">
-        <div className="row">{cardsGrid}</div>
+        <div className="row">
+          <div className="col-9">
+            <div className="row border">{cardsGrid}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
-
+};
 
 export default App;
