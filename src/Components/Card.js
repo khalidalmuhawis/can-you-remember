6aca0e5292f478ea0257ce3ac4d5222cb253f309
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-10-07 17:30:28 +0300

    Done with comparing the cards and keeping a card flipped up if they match

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,39 +1,42 @@
-import React,  { useState } from "react";
+import React, { useState } from "react";
 import cardBack from "../images/CardBack.jpg";
 import ReactCardFlip from "react-card-flip";
 
-
 const Card = ({ card, checkFlipped }) => {
-    const [flipped, changeFlip] = useState(false);
+  const [flipped, changeFlip] = useState(false);
 
-    const handleFlip = () => {
-        changeFlip(!flipped);
-      };
-  
+  const handleFlip = () => {
+    if (flipped !== true) {
+      changeFlip(true);
 
-    return (
-      <div className="col-3 my-1">
+      checkFlipped({ id: card.id, changeFlip: changeFlip });
+    }
+  }; //1
+  return (
+    <div className="col-3 my-1">
       <ReactCardFlip isFlipped={flipped} flipDirection="vertical">
-      <img
+        <img
           className="mx-auto"
           src={cardBack}
+          //   used percentages instead of pixels to be responsive with the screen size
           height="100%"
           width="100%"
           key="front"
-          onClick={() => handleFlip()}
+          onClick={() => handleFlip()} //2
         />
+
         <img
           className="mx-auto"
           src={card.front}
+          //   used percentages instead of pixels to be responsive with the screen size
           height="100%"
           width="100%"
           key="back"
-          onClick={() => handleFlip()}
+          onClick={() => handleFlip()} //2
         />
       </ReactCardFlip>
     </div>
   );
 };
-  
-  
-  export default Card;
+
+export default Card;
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -11,7 +11,9 @@ import Card from "./Card";
 
 const Game = ({ difficulty }) => {
   const [cards, setCards] = useState([]);
+  //   const [flippedCards, changeFlipped] = useState([]);
 
+  //Used to duplicate the amount of cards since we need two of each and shuffle them using the function defined at the top
   useEffect(() => {
     let cards = allCards;
     switch (difficulty) {
@@ -27,15 +29,39 @@ const Game = ({ difficulty }) => {
     setCards(() => shuffle([...cards, ...cards]));
   }, [difficulty]);
 
-  const cardsGrid = cards.map((card, idx) => (
-    <Card key={`${card.id}-${idx}`} card={card} />
+  let flippedCards = [];
+  const changeFlipped = anArray => {
+    flippedCards = anArray;
+  }; //1
+
+  const unflipCards = (unflip1, unflip2) => {
+    setTimeout(() => {
+      unflip1(false);
+      unflip2(false);
+    }, 1000);
+  }; //2
+
+  const checkFlipped = flippedObject => {
+    changeFlipped([...flippedCards, flippedObject]);
+
+    if (flippedCards.length === 2) {
+      if (flippedCards[0].id !== flippedCards[1].id) {
+        unflipCards(flippedCards[0].changeFlip, flippedCards[1].changeFlip);
+      }
+      changeFlipped([]);
+    }
+  }; //3
+
+  //Mapping through the array of cards and placing them in the card component
+  const cardList = cards.map((card, idx) => (
+    <Card key={`${card.id}-${idx}`} card={card} checkFlipped={checkFlipped} /> //4
   ));
 
   return (
     <div className="container">
       <div className="row">
         <div className=" col-9">
-          <div className="row border">{cardsGrid}</div>
+          <div className="row border">{cardList}</div>
         </div>
       </div>
     </div>
